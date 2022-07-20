f5563739f41991a03fbe02e1cd00db6604f0640c
fneathe@gmail.com <fneathe@gmail.com>
2022-07-20 17:29:33 +0900

    main

--- img2grey/grey2img.jsx
+++ img2grey/grey2img.jsx
@@ -10,17 +10,18 @@ function grey2img() {
 		graphics = doc.allGraphics[i];
 		linkName = graphics.itemLink.name;
 		linkPath = graphics.itemLink.filePath;
-		if (linkName.search('.png') > 0) {
+		if (linkName.search('.png') > 0 || linkName.search('.PNG') > 0) {
 			app.pngExportPreferences.pngColorSpace = PNGColorSpaceEnum.GRAY
 			app.pngExportPreferences.pngQuality = PNGQualityEnum.HIGH;
 			app.pngExportPreferences.transparentBackground = true;
 			convertName = new File(linkPath);
 			graphics.exportFile(ExportFormat.PNG_FORMAT, convertName);
-		} else if (linkName.search('.jpg') > 0 || linkName.search('.jpeg') > 0) {
+		} else if (linkName.search('.jpg') > 0 || linkName.search('.jpeg') > 0 || linkName.search('.JPG') > 0 || linkName.search('.JPEG') > 0) {
 			app.jpegExportPreferences.jpegColorSpace = JpegColorSpaceEnum.GRAY
 			app.jpegExportPreferences.jpegQuality = JPEGOptionsQuality.HIGH
 			convertName = new File(linkPath);
 			graphics.exportFile(ExportFormat.JPG, convertName);
 		}
 	}
+	alert("완료합니다. updatelinks 스크립트를 실행하세요.");
 }
--- img2grey/updatelinks.jsx
+++ img2grey/updatelinks.jsx
@@ -8,8 +8,9 @@ function updatelinks() {
 	for (var i=0;i<imgCount;i++) {
 		graphics = doc.allGraphics[i];
 		linkName = graphics.itemLink.name;
-		if (linkName.search('.png') > 0 || linkName.search('.jpg') > 0 || linkName.search('.jpeg') > 0) {
+		if (linkName.search('.png') > 0 || linkName.search('.jpg') > 0 || linkName.search('.jpeg') > 0 || linkName.search('.PNG') > 0 || linkName.search('.JPG') > 0 || linkName.search('.JPEG') > 0) {
 			graphics.itemLink.update();
 		}
 	}
+	alert("완료합니다.");
 }
